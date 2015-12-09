5e2afe967b75f990c808239c65a37872a719d631
LSLdalong <405585058@qq.com>
2015-12-09 17:14:59 +0800

    第一次

---
+++ dalongTest34/dalongTest34/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dalongTest34
+//
+//  Created by 大龙 on 15/12/9.
+//  Copyright © 2015年 大龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dalongTest34/dalongTest34/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dalongTest34
+//
+//  Created by 大龙 on 15/12/9.
+//  Copyright © 2015年 大龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
