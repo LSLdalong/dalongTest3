964d410386393fb7b7a98b65c89548788b49ba1e
xiaoXian3544 <bowang0913@126.com>
2015-12-10 09:48:04 +0800

    //

---
+++ dalongTest34/dalongTest34/weer/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  dalongTest34
+//
+//  Created by 王博 on 15/12/10.
+//  Copyright © 2015年 大龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ dalongTest34/dalongTest34/weer/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  dalongTest34
+//
+//  Created by 王博 on 15/12/10.
+//  Copyright © 2015年 大龙. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
